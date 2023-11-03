c312910070e0a7cd81a0f829a60bedec05e53a39
semyon.falitzyn <semen.falitsin@indusoft.ru>
2023-11-03 16:07:56 +0300

    Init

---
+++ AnyFunctionColorYouLike/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace AnyFunctionColorYouLike;
+
+/// <summary>
+/// Namespace to use.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly)]
+public class NamespaceAttribute : Attribute
+{
+    /// <summary>
+    /// Initialize a new instance of <see cref="NamespaceAttribute"/>
+    /// </summary>
+    public NamespaceAttribute(string @namespace)
+    {
+    }
+}
